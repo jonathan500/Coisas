fe8a40428c7904d21a3c2b2a1779767578e4c8ef
Jonathan Izidoro <tecnologia@contabilivre.com.br>
2021-11-01 10:55:38 -0300

    Upload de arquivos

--- paginas/cadastrot.php
+++ paginas/cadastrot.php
@@ -4,23 +4,67 @@ session_start();
 if(isset($_POST['cadastrart'])){
     $nome = $_POST['nomet'];
     $descricao = $_POST['desct'];
-    $diretorio = "../../arquivos";
+    $diretorio = "../arquivos/";
     $arquivo = $_FILES['tarefa']['name'];
     $data = $_POST['datat'];
+    echo $data;
     $urgencia = $_POST['urgencia'];
     $materia = $_POST['materia'];
-    $iduser;    
+    $dir = $diretorio.md5($arquivo);    
     $sql = $conexao->prepare("SELECT id_usuario FROM tb_usuario WHERE email_usuario = :email AND senha_usuario = :senha");
     $sql->bindValue(':email',$_SESSION['email'], PDO::PARAM_STR);
     $sql->bindValue(':senha',$_SESSION['senha'], PDO::PARAM_STR);
     $sql->execute();
     $pesquisa = $sql->fetch(PDO::PARAM_STR);
+    $id_usu = $pesquisa['id_usuario'];
+    //print_r($pesquisa);
+    //print_r($dir);
+    $sql2 = $conexao->prepare("SELECT id_materia FROM tb_materias WHERE nome_materia = :materia");
+    $sql2->bindValue(':materia',$materia, PDO::PARAM_STR);
+    $sql2->execute();
+    $pesquisa2 = $sql2->fetch(PDO::PARAM_STR);
+    $id_materia = $pesquisa2['id_materia'];
+    //print_r($pesquisa2);
+    try{
+        $sql3 = $conexao->prepare("INSERT INTO tb_tarefas(
+        nome_tarefa, 
+        descricao_tarefa, 
+        arquivo_tarefa, 
+        dataEntrega_tarefa, 
+        urgencia_tarefa, 
+        fk_usuario, 
+        fk_materias) VALUES
+        (
+        :nome, 
+        :descricao, 
+        :arquivo, 
+        :dataEntrega, 
+        :urgencia, 
+        :idUsu, 
+        :idMat)");
+        $sql3->bindValue(':nome',$nome, PDO::PARAM_STR);
+        $sql3->bindValue(':descricao',$descricao, PDO::PARAM_STR);
+        $sql3->bindValue(':arquivo', $dir, PDO::PARAM_STR);
+        $sql3->bindValue(':dataEntrega',$data, PDO::PARAM_STR);
+        $sql3->bindValue(':urgencia',$urgencia, PDO::PARAM_STR);
+        $sql3->bindValue(':idUsu',$id_usu, PDO::PARAM_STR);
+        $sql3->bindValue(':idMat',$id_materia, PDO::PARAM_STR);
+        $sql3->execute();
+        $pesquisa3 = $sql3->fetch(PDO::PARAM_STR);
+        if(empty($pesquisa3)){
+            echo "";
+        }else{
+            echo "deu bom";
+        }
+    }catch(PDOException $e){
+        
+    }
     
 }else{
     echo "algo deu de errado";
 }
 
-
+//move_uploaded_file($_FILES['tarefa']['tmp_name'],$diretorio.md5($arquivo));
 ?>
 
 <!DOCTYPE html>
